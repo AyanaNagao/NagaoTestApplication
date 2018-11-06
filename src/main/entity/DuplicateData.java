280d8702ee4d3d4ccd6efdd9d9aeb686b4269cbb
AyanaNagao <ayana.nagao@randstad.co.jp>
2018-11-06 11:51:45 +0900

    Upload
    little bit clean :D

---
+++ src/main/entity/DuplicateData.java
@@ -0,0 +1,249 @@
+package main.entity;
+
+public class DuplicateData {
+
+	private String staffCd1;
+	private String staffCd2;
+	private String firstNm1;
+	private String firstNm2;
+	private String firstNmKn1;
+	private String firstNmKn2;
+	private String lastNm1;
+	private String lastNm2;
+	private String lastNmKn1;
+	private String lastNmKn2;
+	private String oldFirstNm1;
+	private String oldFirstNm2;
+	private String oldFirstNmKn1;
+	private String oldFirstNmKn2;
+	private String birthDay1;
+	private String birthDay2;
+	private String telNum1;
+	private String telNum2;
+	private String caryTelNum1;
+	private String caryTelNum2;
+	private String mail1_1;
+	private String mail1_2;
+	private String mail2_1;
+	private String mail2_2;
+	private String address1;
+	private String address2;
+	private String duplicateData;
+
+	public String getStaffCd1() {
+		return staffCd1;
+	}
+
+	public void setStaffCd1(String staffCd1) {
+		this.staffCd1 = staffCd1;
+	}
+
+	public String getStaffCd2() {
+		return staffCd2;
+	}
+
+	public void setStaffCd2(String staffCd2) {
+		this.staffCd2 = staffCd2;
+	}
+
+	public String getFirstNm1() {
+		return firstNm1;
+	}
+
+	public void setFirstNm1(String firstNm1) {
+		this.firstNm1 = firstNm1;
+	}
+
+	public String getFirstNm2() {
+		return firstNm2;
+	}
+
+	public void setFirstNm2(String firstNm2) {
+		this.firstNm2 = firstNm2;
+	}
+
+	public String getLastNm1() {
+		return lastNm1;
+	}
+
+	public void setLastNm1(String lastNm1) {
+		this.lastNm1 = lastNm1;
+	}
+
+	public String getLastNm2() {
+		return lastNm2;
+	}
+
+	public void setLastNm2(String lastNm2) {
+		this.lastNm2 = lastNm2;
+	}
+
+	public String getOldFirstNm1() {
+		return oldFirstNm1;
+	}
+
+	public void setOldFirstNm1(String oldFirstNm1) {
+		this.oldFirstNm1 = oldFirstNm1;
+	}
+
+	public String getOldFirstNm2() {
+		return oldFirstNm2;
+	}
+
+	public void setOldFirstNm2(String oldFirstNm2) {
+		this.oldFirstNm2 = oldFirstNm2;
+	}
+
+	public String getTelNum1() {
+		return telNum1;
+	}
+
+	public void setTelNum1(String telNum1) {
+		this.telNum1 = telNum1;
+	}
+
+	public String getTelNum2() {
+		return telNum2;
+	}
+
+	public void setTelNum2(String telNum2) {
+		this.telNum2 = telNum2;
+	}
+
+	public String getCaryTelNum1() {
+		return caryTelNum1;
+	}
+
+	public void setCaryTelNum1(String caryTelNum1) {
+		this.caryTelNum1 = caryTelNum1;
+	}
+
+	public String getCaryTelNum2() {
+		return caryTelNum2;
+	}
+
+	public void setCaryTelNum2(String caryTelNum2) {
+		this.caryTelNum2 = caryTelNum2;
+	}
+
+	public String getMail1_1() {
+		return mail1_1;
+	}
+
+	public void setMail1_1(String mail1_1) {
+		this.mail1_1 = mail1_1;
+	}
+
+	public String getMail1_2() {
+		return mail1_2;
+	}
+
+	public void setMail1_2(String mail1_2) {
+		this.mail1_2 = mail1_2;
+	}
+
+	public String getMail2_1() {
+		return mail2_1;
+	}
+
+	public void setMail2_1(String mail2_1) {
+		this.mail2_1 = mail2_1;
+	}
+
+	public String getMail2_2() {
+		return mail2_2;
+	}
+
+	public void setMail2_2(String mail2_2) {
+		this.mail2_2 = mail2_2;
+	}
+
+	public String getAddress1() {
+		return address1;
+	}
+
+	public void setAddress1(String address1) {
+		this.address1 = address1;
+	}
+
+	public String getAddress2() {
+		return address2;
+	}
+
+	public void setAddress2(String address2) {
+		this.address2 = address2;
+	}
+
+	public String getFirstNmKn1() {
+		return firstNmKn1;
+	}
+
+	public void setFirstNmKn1(String firstNmKn1) {
+		this.firstNmKn1 = firstNmKn1;
+	}
+
+	public String getFirstNmKn2() {
+		return firstNmKn2;
+	}
+
+	public void setFirstNmKn2(String firstNmKn2) {
+		this.firstNmKn2 = firstNmKn2;
+	}
+
+	public String getLastNmKn1() {
+		return lastNmKn1;
+	}
+
+	public void setLastNmKn1(String lastNmKn1) {
+		this.lastNmKn1 = lastNmKn1;
+	}
+
+	public String getLastNmKn2() {
+		return lastNmKn2;
+	}
+
+	public void setLastNmKn2(String lastNmKn2) {
+		this.lastNmKn2 = lastNmKn2;
+	}
+
+	public String getOldFirstNmKn1() {
+		return oldFirstNmKn1;
+	}
+
+	public void setOldFirstNmKn1(String oldFirstNmKn1) {
+		this.oldFirstNmKn1 = oldFirstNmKn1;
+	}
+
+	public String getOldFirstNmKn2() {
+		return oldFirstNmKn2;
+	}
+
+	public void setOldFirstNmKn2(String oldFirstNmKn2) {
+		this.oldFirstNmKn2 = oldFirstNmKn2;
+	}
+
+	public String getDuplicateData() {
+		return duplicateData;
+	}
+
+	public void setDuplicateData(String duplicateData) {
+		this.duplicateData = duplicateData;
+	}
+
+	public String getBirthDay1() {
+		return birthDay1;
+	}
+
+	public void setBirthDay1(String birthDay1) {
+		this.birthDay1 = birthDay1;
+	}
+
+	public String getBirthDay2() {
+		return birthDay2;
+	}
+
+	public void setBirthDay2(String birthDay2) {
+		this.birthDay2 = birthDay2;
+	}
+
+}
---
+++ src/main/entity/StaffEntity.java
@@ -0,0 +1,123 @@
+package main.entity;
+
+public class StaffEntity {
+
+	private String staffCd;
+	private String firstNm;
+	private String lastNm;
+	private String firstNmKn;
+	private String lastNmKn;
+	private String oldLastNm;
+	private String oldLastNmKn;
+	private String brthDay;
+	private String telNam;
+	private String caryTelNam;
+	private String mail1;
+	private String mail2;
+	private String address;
+
+	public String getFirstNm() {
+		return firstNm;
+	}
+
+	public void setFirstNm(String firstNm) {
+		this.firstNm = firstNm;
+	}
+
+	public String getLastNm() {
+		return lastNm;
+	}
+
+	public void setLastNm(String lastNm) {
+		this.lastNm = lastNm;
+	}
+
+	public String getFirstNmKn() {
+		return firstNmKn;
+	}
+
+	public void setFirstNmKn(String firstNmKn) {
+		this.firstNmKn = firstNmKn;
+	}
+
+	public String getLastNmKn() {
+		return lastNmKn;
+	}
+
+	public void setLastNmKn(String lastNmKn) {
+		this.lastNmKn = lastNmKn;
+	}
+
+	public String getOldLastNm() {
+		return oldLastNm;
+	}
+
+	public void setOldLastNm(String oldLastNm) {
+		this.oldLastNm = oldLastNm;
+	}
+
+	public String getOldLastNmKn() {
+		return oldLastNmKn;
+	}
+
+	public void setOldLastNmKn(String oldLastNmKn) {
+		this.oldLastNmKn = oldLastNmKn;
+	}
+
+	public String getBrthDay() {
+		return brthDay;
+	}
+
+	public void setBrthDay(String brthDay) {
+		this.brthDay = brthDay;
+	}
+
+	public String getTelNam() {
+		return telNam;
+	}
+
+	public void setTelNam(String telNam) {
+		this.telNam = telNam;
+	}
+
+	public String getCaryTelNam() {
+		return caryTelNam;
+	}
+
+	public void setCaryTelNam(String caryTelNam) {
+		this.caryTelNam = caryTelNam;
+	}
+
+	public String getMail1() {
+		return mail1;
+	}
+
+	public void setMail1(String mail1) {
+		this.mail1 = mail1;
+	}
+
+	public String getMail2() {
+		return mail2;
+	}
+
+	public void setMail2(String mail2) {
+		this.mail2 = mail2;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getStaffCd() {
+		return staffCd;
+	}
+
+	public void setStaffCd(String staffCd) {
+		this.staffCd = staffCd;
+	}
+
+}
